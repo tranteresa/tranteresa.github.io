1f799dc937e20e4f8c83276c4a3a92306aedf406
tranteresa <tranteresaat@gmail.com>
2018-05-21 20:05:04 -0700

    Add CSS Styling to CS-Cases

--- scripts/cs-happypaws.js
+++ scripts/cs-happypaws.js
@@ -4,3 +4,17 @@
 function openWinPersonas() {
     window.open("assets/cs-happypaws/personas.pdf");
 }
+
+// Sticky Menu
+window.onscroll = function() {myFunction()};
+
+var stickymenu = document.getElementById("stickymenu");
+var sticky = stickymenu.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    stickymenu.classList.add("sticky")
+  } else {
+    stickymenu.classList.remove("sticky");
+  }
+}
